eb28a9c3a4c3bdbc6a7fda9a36c51e31f674146b
centny <centny@gmail.com>
2022-11-03 16:15:55 +0800

    move shoulder client.

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/codingeasygo/web
 go 1.18
 
 require (
-	github.com/codingeasygo/util v0.0.0-20220823140802-c3d6b044d914
+	github.com/codingeasygo/util v0.0.0-20221103081314-a6c91ccf3379
 	golang.org/x/net v0.0.0-20210503060351-7fd8e65b6420
 )
--- httptest/httptest.go
+++ httptest/httptest.go
@@ -13,99 +13,6 @@ import (
 	"github.com/codingeasygo/web"
 )
 
-type Client struct {
-	Shoulder xmap.Shoulder
-	Client   *xhttp.Client
-}
-
-//Should will assert by xmap.M.Should
-func (c *Client) Should(t *testing.T, args ...interface{}) *Client {
-	c.Shoulder.Should(t, args...)
-	return c
-}
-
-//ShouldError will assert err is not nil
-func (c *Client) ShouldError(t *testing.T) *Client {
-	c.Shoulder.ShouldError(t)
-	return c
-}
-
-//OnlyLog will only show error log
-func (c *Client) OnlyLog(only bool) *Client {
-	c.Shoulder.OnlyLog(only)
-	return c
-}
-
-//GetMap will get map from remote
-func (c *Client) GetMap(format string, args ...interface{}) (data xmap.M, err error) {
-	data, err = c.Client.GetMap(format, args...)
-	c.Shoulder.Valid(3, data, err)
-	return
-}
-
-//GetHeaderMap will get map from remote
-func (c *Client) GetHeaderMap(header xmap.M, format string, args ...interface{}) (data xmap.M, res *http.Response, err error) {
-	data, res, err = c.Client.GetHeaderMap(header, format, args...)
-	c.Shoulder.Valid(3, data, err)
-	return
-}
-
-//PostMap will get map from remote
-func (c *Client) PostMap(body io.Reader, format string, args ...interface{}) (data xmap.M, err error) {
-	data, err = c.Client.PostMap(body, format, args...)
-	c.Shoulder.Valid(3, data, err)
-	return
-}
-
-//PostTypeMap will get map from remote
-func (c *Client) PostTypeMap(contentType string, body io.Reader, format string, args ...interface{}) (data xmap.M, err error) {
-	data, err = c.Client.PostTypeMap(contentType, body, format, args...)
-	c.Shoulder.Valid(3, data, err)
-	return
-}
-
-//PostHeaderMap will get map from remote
-func (c *Client) PostHeaderMap(header xmap.M, body io.Reader, format string, args ...interface{}) (data xmap.M, res *http.Response, err error) {
-	data, res, err = c.Client.PostHeaderMap(header, body, format, args...)
-	c.Shoulder.Valid(3, data, err)
-	return
-}
-
-//PostJSONMap will get map from remote
-func (c *Client) PostJSONMap(body interface{}, format string, args ...interface{}) (data xmap.M, err error) {
-	data, err = c.Client.PostJSONMap(body, format, args...)
-	c.Shoulder.Valid(3, data, err)
-	return
-}
-
-//MethodBytes will do http request, read reponse and parse to map
-func (c *Client) MethodMap(method string, header xmap.M, body io.Reader, format string, args ...interface{}) (data xmap.M, res *http.Response, err error) {
-	data, res, err = c.Client.MethodMap(method, header, body, format, args...)
-	c.Shoulder.Valid(3, data, err)
-	return
-}
-
-//PostFormMap will get map from remote
-func (c *Client) PostFormMap(form xmap.M, format string, args ...interface{}) (data xmap.M, err error) {
-	data, err = c.Client.PostFormMap(form, format, args...)
-	c.Shoulder.Valid(3, data, err)
-	return
-}
-
-//PostMultipartMap will get map from remote
-func (c *Client) PostMultipartMap(header, fields xmap.M, format string, args ...interface{}) (data xmap.M, err error) {
-	data, err = c.Client.PostMultipartMap(header, fields, format, args...)
-	c.Shoulder.Valid(3, data, err)
-	return
-}
-
-//UploadMap will get map from remote
-func (c *Client) UploadMap(fields xmap.M, filekey, filename, format string, args ...interface{}) (data xmap.M, err error) {
-	data, err = c.Client.UploadMap(fields, filekey, filename, format, args...)
-	c.Shoulder.Valid(3, data, err)
-	return
-}
-
 //Server is httptest server
 type Server struct {
 	*xhttp.Client
@@ -169,16 +76,14 @@ func (s *Server) rawRequest(method, uri string, header xmap.M, body io.Reader) (
 	return
 }
 
-func (s *Server) Should(t *testing.T, args ...interface{}) *Client {
-	c := &Client{
-		Client: &xhttp.Client{Raw: s.rawRequest},
-	}
+func (s *Server) Should(t *testing.T, args ...interface{}) *xhttp.ShouldClient {
+	c := xhttp.NewShouldClient()
+	c.Client = &xhttp.Client{Raw: s.rawRequest}
 	return c.Should(t, args...)
 }
 
-func (s *Server) ShouldError(t *testing.T) *Client {
-	c := &Client{
-		Client: &xhttp.Client{Raw: s.rawRequest},
-	}
+func (s *Server) ShouldError(t *testing.T) *xhttp.ShouldClient {
+	c := xhttp.NewShouldClient()
+	c.Client = &xhttp.Client{Raw: s.rawRequest}
 	return c.ShouldError(t)
 }
